4e9bec1d2b94393e29ac7245e24b10fd933659b4
Hortensjaa <lilka305@gmail.com>
2023-02-02 19:59:27 +0100

    confirmation emails sending added

--- MoriaVisuals/settings.py
+++ MoriaVisuals/settings.py
@@ -3,19 +3,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Secret key
 from decouple import config
-
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Debug
 DEBUG = True
 
+# Hosts
 ALLOWED_HOSTS = []
+
+# User model is customer
 AUTH_USER_MODEL = "customers.Customer"
 
 # Application definition
@@ -66,7 +64,6 @@ WSGI_APPLICATION = 'MoriaVisuals.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -77,7 +74,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -96,7 +92,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -108,14 +103,22 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Logging
+
 LOGIN_REDIRECT_URL = "products:home_page"
 LOGOUT_REDIRECT_URL = "products:home_page"
+
+# Emails
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
+from django.core import mail
+from django.conf import settings
+import smtplib
+import ssl
+from email.message import EmailMessage
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
@@ -35,16 +40,42 @@ def confirm_address(request):
         return render(request, 'orders/confirm_address.html', {'address': customer.address})
 
 
-# TODO: sending confirmation emails
+# sending a confirmation email after successful placing an order
+def send_confirmation_email(order, customer):
+    products_list = [
+        str(item[0]) + ' x' + str(item[1]) + '\n'
+        for item in order.order_items.values_list('product__product__name', 'count')
+    ]
+    msg = EmailMessage()
+    msg.set_content(
+        f"Hi {customer}!\n"
+        f"Thank you for placing order no {order.id} with: \n{''.join(products_list)}"
+        f"Estimated delivery time is {(order.order_date + timedelta(days=3)).date()}."
+        f"If you have any questions, contact us by this email address."
+    )
+    msg["Subject"] = "Your Moria order confirmation"
+    msg["From"] = "Moria Visuals"
+    msg["To"] = customer.email
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP(settings.EMAIL_HOST, port=settings.EMAIL_PORT) as smtp:
+        smtp.starttls(context=context)
+        smtp.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+        smtp.send_message(msg)
+
+
 # view after successfully placing an order
 def order_confirmed(request, address_id):
     customer = request.user
     address = Address.objects.get(id=address_id)
     order = Order.objects.create(address=address, customer=customer)
-    product_in_cart = CartItem.objects.select_related().filter(customer=customer)
-    if product_in_cart:
-        OrderItem.objects.create(count=product_in_cart[0].count, product=product_in_cart[0].product, order=order)
-        product_in_cart[0].delete()
+    products_in_cart = CartItem.objects.select_related().filter(customer=customer)
+    if products_in_cart:
+        for item in products_in_cart:
+            OrderItem.objects.create(count=item.count, product=item.product, order=order)
+            item.delete()
     customer.last_purchase = datetime.now()
     customer.save()
+    send_confirmation_email(order, customer)
     return render(request, 'orders/order_confirmed.html', {'customer': customer})
